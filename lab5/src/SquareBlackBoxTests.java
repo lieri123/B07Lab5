cf6d3336f96790e991f18ba0fdcf1c5a6500cc0e
kiruha17 <160156478+kiruha17@users.noreply.github.com>
2024-10-27 23:06:03 -0400

    Fix syntax errors in SquareBlackBoxTests.java

--- lab5/src/SquareBlackBoxTests.java
+++ lab5/src/SquareBlackBoxTests.java
@@ -5,21 +5,13 @@ import org.junit.jupiter.api.Test;
 class SquareBlackBoxTests {
 
 	@Test
-	void testNoArgConstructor() {
-		Point origin = new Point(0, 0);
-		Square sq1 = new Square();
-		assertEquals(sq1.A, origin);
-		assertEquals(sq1.length, 1);
-	}
-	
-	@Test
 	void testArgConstructor() {
 		Point A = new Point(2, 5);
 		Square sq1 = new Square(A, 2.7);
 		assertEquals(sq1.A, A);
 		assertEquals(sq1.length, 2.7);
 	}
-	
+
 	@Test
 	void testArea() {
 		Point A = new Point(0, 0);
@@ -31,7 +23,7 @@ class SquareBlackBoxTests {
 		Square sq3 = new Square(A, 0);
 		assertEquals(sq3.area(), 0);
 	}
-	
+
 	@Test
 	void testPerimeter() {
 		Point A = new Point(-2.5, -2.5);
@@ -40,55 +32,58 @@ class SquareBlackBoxTests {
 		Square sq2 = new Square(A, 0);
 		assertEquals(sq2.perimter(), 0);
 	}
-	
+
 	@Test
 	void testEqualsSame() {
 		Point A = new Point(0, 0);
 		Square sq1 = new Square(A, 1);
-		Square sq2 = new Square();
+		Square sq2 = new Square(A, 1);
 		assertTrue(sq1.equals(sq2));
 	}
 
 	@Test
 	void testEqualsDiffPoint() {
-		Point B = new point(1, 1);
-		Square sq1 = new Square();
+		Point A = new Point(0, 0);
+		Point B = new Point(1, 1);
+		Square sq1 = new Square(A, 1);
 		Square sq2 = new Square(B, 1);
 		assertFalse(sq1.equals(sq2));
 	}
-	
+
 	@Test
 	void testEqualsDiffLength() {
-		Square sq1 = new Square();
 		Point A = new Point(0, 0);
+		Square sq1 = new Square(A, 1);
 		Square sq2 = new Square(A, 1.1);
 		assertFalse(sq1.equals(sq2));
 	}
-	
+
 	@Test
 	void testEqualsDiffBoth() {
 		Point A = new Point(1, 0);
-		Square sq1 = new Square();
+		Point B = new Point(0, 0);
+		Square sq1 = new Square(B, 1);
 		Square sq2 = new Square(A, 5);
 		assertFalse(sq1.equals(sq2));
 	}
-	
+
 	@Test
 	void testHashCode() {
 		Point A = new Point(0, 0);
 		Square sq1 = new Square(A, 1);
-		Square sq2 = new Square();
-		assertEquals(sq1.hashCode, sq2.hashCode);
+		Square sq2 = new Square(A, 1);
+		assertEquals(sq1.hashCode(), sq2.hashCode());
 		Point B = new Point(3, 7);
 		Square sq3 = new Square(B, 10);
 		Square sq4 = new Square(B, 10);
-		assertEquals(sq3.hashCode, sq4.hashCode);
+		assertEquals(sq3.hashCode(), sq4.hashCode());
 	}
-	
+
 	@Test
 	void testToString() {
 		Point A = new Point(2, 3);
-		Square sq1 = new Square();
+		Point B = new Point(0, 0);
+		Square sq1 = new Square(B, 1);
 		Square sq2 = new Square(A, 3.7);
 		String exp1 = "Square: (0,0), 1";
 		String exp2 = "Square: (2,3), 3.7";
